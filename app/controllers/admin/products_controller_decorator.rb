3e37c44fc21fe672966e65cd9d41a377bc7cb79d
Fabien Franzen <info@atelierfabien.be>
2011-11-27 12:12:38 +0100

    Initial commit

---
+++ app/controllers/admin/products_controller_decorator.rb
@@ -0,0 +1,22 @@
+Admin::ProductsController.class_eval do
+  
+  protected
+  
+  def assign_option_values
+    option_values = params[:option_values] || {}
+    @object.master.option_values = option_values.values.reject { |id| id.blank? }.map { |id| OptionValue.find(id) }
+  end
+  
+  alias_method(:create_before_without_option_values, :create_before) if method_defined?(:create_before) && !method_defined?(:create_before_without_option_values)
+  def create_before
+    assign_option_values
+    create_before_without_option_values if self.class.method_defined?(:create_before_without_option_values)
+  end
+
+  alias_method(:update_before_without_option_values, :update_before) if method_defined?(:update_before) && !method_defined?(:update_before_without_option_values)
+  def update_before
+    assign_option_values
+    update_before_without_option_values if self.class.method_defined?(:update_before_without_option_values)
+  end
+
+end
---
+++ app/overrides/product_options.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+  :virtual_path => "admin/products/_form",
+  :name => "master_options",
+  :insert_top => "[data-hook='admin_product_form_right']",
+  :partial => "admin/products/master_options",
+  :disabled => false)
