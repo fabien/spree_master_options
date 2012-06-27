12c1bf3e3e6794aeaf3ee41b229d612e39aa2898
Fabien Franzen <info@atelierfabien.be>
2012-06-27 19:31:20 +0200

    Fixed controller decorator

--- app/controllers/admin/products_controller_decorator.rb
+++ app/controllers/admin/products_controller_decorator.rb
@@ -1,22 +1,11 @@
-Admin::ProductsController.class_eval do
-  
-  protected
+Spree::Admin::ProductsController.class_eval do
   
   def assign_option_values
     option_values = params[:option_values] || {}
-    @object.master.option_values = option_values.values.reject { |id| id.blank? }.map { |id| OptionValue.find(id) }
-  end
-  
-  alias_method(:create_before_without_option_values, :create_before) if method_defined?(:create_before) && !method_defined?(:create_before_without_option_values)
-  def create_before
-    assign_option_values
-    create_before_without_option_values if self.class.method_defined?(:create_before_without_option_values)
-  end
-
-  alias_method(:update_before_without_option_values, :update_before) if method_defined?(:update_before) && !method_defined?(:update_before_without_option_values)
-  def update_before
-    assign_option_values
-    update_before_without_option_values if self.class.method_defined?(:update_before_without_option_values)
+    @object.master.option_values = option_values.values.reject { |id| id.blank? }.map { |id| Spree::OptionValue.find(id) }
   end
 
 end
+
+Spree::Admin::ProductsController.create.before :assign_option_values
+Spree::Admin::ProductsController.update.before :assign_option_values
